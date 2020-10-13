14012f654df03e7f0a76c0e525eed53d82dbe83f
TensShinet <tanshunwork@gmail.com>
2020-10-13 08:36:17 +0800

    auth 服务注册初始化

---
+++ service/auth/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"github.com/TensShinet/WeFile/conf"
+	"github.com/TensShinet/WeFile/logging"
+	"github.com/TensShinet/WeFile/service/auth/handler"
+	"github.com/TensShinet/WeFile/service/auth/proto"
+	"github.com/micro/go-micro/v2"
+	"github.com/micro/go-micro/v2/registry"
+	"github.com/micro/go-micro/v2/registry/etcd"
+)
+
+var logger = logging.GetLogger("auth_service")
+
+func startRPCService() {
+	// 初始化配置
+	conf.Init("auth_conf.yml")
+	config := conf.GetConfig()
+	micReg := etcd.NewRegistry(func(options *registry.Options) {
+		options.Addrs = config.Etcd.EndPoints
+	})
+	// 新建服务
+	service := micro.NewService(
+		micro.Name("go.micro.service.auth"),
+		micro.Registry(micReg),
+		micro.Version("latest"),
+	)
+	// 服务初始化
+	service.Init()
+
+	// 注册服务
+	if err := proto.RegisterServiceHandler(service.Server(), new(handler.Service)); err != nil {
+		logger.Panicf("register failed, for the reason:%v", err.Error())
+	}
+	// 启动服务
+	if err := service.Run(); err != nil {
+		logger.Panicf("id generator start failed, for the reason:%v", err.Error())
+	}
+}
+
+// jwt 相关
+// jwt encode: 将一些关键信息 encode 之后返回
+// jwt decode: 将一些关键信息 decode 之后返回，判断是否过期，判断签名是否一致
+func main() {
+	startRPCService()
+}
